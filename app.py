ceffd72b5338b64f578f01e1fa8518f75e293db7
rujul007 <106137481+rujul007@users.noreply.github.com>
2022-05-30 18:56:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+
+@app.route("/sms", methods=['post'])
+def reply():
+    print ("1")
+    text = request.form.get("Body")
+    number = request.form.get("From")
+    number = number.replace("whatsapp:", "")
+    response = MessagingResponse()
+    print("2")
+    response.message(f"माझ्याशी संपर्क  धन्यवाद. तुम्ही पाठवले आहे '{text}' या क्रमांकावरून {number[-10:]}")
+    print("3")
+    return str(response)
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
